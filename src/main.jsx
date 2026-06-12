89973951761a9c33bee746fb19db6f2b2ba7ea7a
Bluff Distillery <info@bluffdistillery.com>
2026-06-12 00:11:36 +0000

    Manual code change

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+console.log('APP_ID', import.meta.env.VITE_BASE44_APP_ID);
+console.log('BASE_URL', import.meta.env.VITE_BASE44_APP_BASE_URL);
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
